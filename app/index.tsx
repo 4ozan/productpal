5a1e2d5d6ef2a405800ed0131feefe1cd4b442c1
fawuzantech <ibrahimpima76@gmail.com>
2025-06-26 02:04:57 +0000

    was adding background image

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,16 @@
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, ImageBackground,Text, Button, StyleSheet } from 'react-native';
 import { router } from 'expo-router';
-
 export default function LandingScreen() {
   const navigateToApp = () => {
-    // Replace the current screen so the user cannot go back to it
     router.replace('/(tabs)');
   };
 
   return (
     <View style={styles.container}>
+      <ImageBackground source={require('https://unsplash.com/photos/a-painting-of-a-man-holding-a-palette-ypTyyuxhbzE.jpg')} style={styles.image} >
       <Text style={styles.title}>Landing</Text>
       <Button title="Start chatting" onPress={navigateToApp} />
+      </ImageBackground>
     </View>
   );
 }
@@ -25,4 +25,11 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 16,
   },
+  image: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    resizeMode:'cover',
+  }
+  
 });
