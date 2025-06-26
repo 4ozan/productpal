47f8e0f92c746e79aa666ac9df3eca63d797dc41
fawuzantech <ibrahimpima76@gmail.com>
2025-06-26 02:10:45 +0000

    minor cleanups

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,29 @@
-import { View, ImageBackground,Text, Button, StyleSheet } from 'react-native';
-import { router } from 'expo-router';
-export default function LandingScreen() {
+import React from "react";
+import { View, ImageBackground, Text, Button, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+const LandingScreen: React.FC = () => {
+  const router = useRouter();
+
   const navigateToApp = () => {
-    router.replace('/(tabs)');
+    router.replace("/(tabs)");
   };
 
   return (
     <View style={styles.container}>
-      <ImageBackground source={require('https://unsplash.com/photos/a-painting-of-a-man-holding-a-palette-ypTyyuxhbzE.jpg')} style={styles.image} >
+      <ImageBackground
+        source={{
+          uri: "https://images.unsplash.com/photo-1535909339361-9fef1f0ef41f?auto=format&fit=crop&w=800&q=80",
+        }}
+        style={styles.image}
+      >
       <Text style={styles.title}>Landing</Text>
       <Button title="Start chatting" onPress={navigateToApp} />
       </ImageBackground>
     </View>
   );
-}
+};
+
+export default LandingScreen;
 
 const styles = StyleSheet.create({
   container: {
